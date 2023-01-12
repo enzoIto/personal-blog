aa835cee3937d4ff4b18e190681aead984144a46
Enzo Massaki Ito <enzo.ito@MM-NB-FVFH407MQ6LR.local>
2023-01-11 21:45:25 -0300

    basic landing page

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,16 @@
 use yew::prelude::*;
 
 #[function_component]
-fn App() -> Html {
-    let counter = use_state(|| 0);
-    let onclick = {
-        let counter = counter.clone();
-        move |_| {
-            let value = *counter + 1;
-            counter.set(value);
-        }
-    };
-
+fn LandingPage() -> Html {
     html! {
-        <div>
-            <button {onclick}>{ "+1" }</button>
-            <p>{ *counter }</p>
+        <div class="container">
+            <h1>{"Welcome to the Rust Blog"}</h1>
+            <p>{"This is a blog application built using Rust and Yew"}</p>
+            <a href="/posts" class="button">{"View Posts"}</a>
         </div>
     }
 }
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+    yew::Renderer::<LandingPage>::new().render();
 }
